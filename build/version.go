421bd9c691e54fc86faa217e538334a3ffaeb7a0
Paul <paul@the7thdream.com>
2026-07-12 02:42:04 +0800

    Tool running indicator

--- build/version.go
+++ build/version.go
@@ -3,7 +3,7 @@ package build
 
 // Version is the current version of llmrouter
 // This should be updated for each release
-const Version = "0.6.0"
+const Version = "0.6.1"
 
 // BuildDate can be set at compile time using ldflags
 // e.g., go build -ldflags "-X github.com/paularlott/llmrouter/build.BuildDate=$(date -u +%Y-%m-%dT%H:%M:%SZ)"
